8ff33665b23605a4208d6dcc5240bf1bd5727444
etu <bourgon.nils@gmail.com>
2019-04-04 14:15:19 +0200

    add FileReader

--- src/Grille.java
+++ src/Grille.java
@@ -1,20 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Grille {
 	
-	private HashMap<Coord,Pions> pions;
+	static public final Character opponent ='X';
+	static public final Character ally = 'O';
+	static public final Character emptyCell ='_';
+	
+	private HashMap<Coord,Character> pions;
 	
 	public Grille(){
 		this.pions = new HashMap();
 	}
 	
 	public Grille(Grille gr){
-		...
+		
 	}
 	
-	public void addPion(Coord c,Pions p){
-		this.pions.add(c,p);
+	public void addPion(Coord c,Character opponent2){
+		this.pions.put(c,opponent2);
 	}
 	
 	public int eval(Pions types){
+		return 0;
+	}
+	
+	public static Grille readGrille(File path) throws IOException{
+		Character[] charPossibles = {opponent, ally,emptyCell};
+		Grille ret =new Grille();
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String line;
+		for(int x = 0; x<7; x++) {
+			line=br.readLine();
+			for (int y =0; y<6;y++) {
+				Character current = line.charAt(y);
+				if(current == opponent || current == ally) {
+					ret.addPion(new Coord(x, y),current);
+				}
+			}
+		}
+		return ret;
 
 	}
 
